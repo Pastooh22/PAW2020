446f00eb9f8d72f4e5da531f3864bd97eb2d4bd2
Pastooh22 <48555920+Pastooh22@users.noreply.github.com>
2020-04-23 10:04:49 +0200

    fixed

--- tictactoe/tictactoe.js
+++ tictactoe/tictactoe.js
@@ -1,40 +1,111 @@
+var State;
+(function (State) {
+    State[State["Empty"] = 0] = "Empty";
+    State[State["X"] = 1] = "X";
+    State[State["O"] = 2] = "O";
+})(State || (State = {}));
 var Cell = /** @class */ (function () {
-    function Cell(cell) {
-        this.htmlElement = cell;
+    function Cell() {
+        this.state = State.Empty;
     }
-    Cell.prototype.SetStatus = function (status) {
-        this.cellValue = status;
-        if (status === -1) {
-            this.htmlElement.innerHTML = "O";
-        }
-        else if (status === 1) {
-            this.htmlElement.innerHTML = "X";
-        }
+    Cell.prototype.SetState = function (s) {
+        this.state = s;
     };
     return Cell;
 }());
 var Board = /** @class */ (function () {
-    function Board() {
-        this.currentSymbol = -1;
-        this.cells = new Array(9);
-        this.cells[0] = new Cell(document.getElementById("cell11"));
-        this.cells[1] = new Cell(document.getElementById("cell12"));
-        this.cells[2] = new Cell(document.getElementById("cell13"));
-        this.cells[3] = new Cell(document.getElementById("cell21"));
-        this.cells[4] = new Cell(document.getElementById("cell22"));
-        this.cells[5] = new Cell(document.getElementById("cell23"));
-        this.cells[6] = new Cell(document.getElementById("cell31"));
-        this.cells[7] = new Cell(document.getElementById("cell32"));
-        this.cells[8] = new Cell(document.getElementById("cell33"));
+    function Board(t) {
+        this.table = t;
+        this.p1Turn = true;
+        this.board = [new Cell, new Cell, new Cell, new Cell, new Cell, new Cell, new Cell, new Cell, new Cell];
+        this.score = document.getElementById("score");
+        this.p1Score = 0;
+        this.p2Score = 0;
     }
-    Board.prototype.makeMove = function (cellNumber) {
-        this.cells[cellNumber].SetStatus(this.currentSymbol);
-        this.currentSymbol = this.currentSymbol === -1 ? 1 : -1;
-        return Board;
+    Board.prototype.click = function (c) {
+        if (this.board[c].state == State.Empty) {
+            if (this.p1Turn) {
+                this.board[c].SetState(State.X);
+                this.table[c].innerHTML = "X";
+            }
+            else {
+                this.board[c].SetState(State.O);
+                this.table[c].innerHTML = "O";
+            }
+            if (this.hWin(this.board) || this.vWin(this.board) || this.dWin(this.board)) {
+                if (this.p1Turn == true) {
+                    this.p1Score++;
+                }
+                else {
+                    this.p2Score++;
+                }
+                this.printScore();
+                this.resetGame();
+            }
+            if (this.draw()) {
+                this.resetGame();
+            }
+            this.p1Turn = !this.p1Turn;
+        }
     };
-    Board.prototype.let = function () {
-        document.getElementById("cell11").innerHTML = "Hello World";
+    Board.prototype.hWin = function (b) {
+        if (b[0].state == b[1].state && b[1].state == b[2].state && b[1].state != State.Empty ||
+            b[3].state == b[4].state && b[4].state == b[5].state && b[4].state != State.Empty ||
+            b[6].state == b[7].state && b[7].state == b[8].state && b[7].state != State.Empty) {
+            return true;
+        }
+        return false;
+    };
+    Board.prototype.vWin = function (b) {
+        if (b[0].state == b[3].state && b[3].state == b[6].state && b[3].state != State.Empty ||
+            b[1].state == b[4].state && b[4].state == b[7].state && b[4].state != State.Empty ||
+            b[2].state == b[5].state && b[5].state == b[8].state && b[5].state != State.Empty) {
+            return true;
+        }
+        return false;
+    };
+    Board.prototype.dWin = function (b) {
+        if (b[0].state == b[4].state && b[4].state == b[8].state && b[4].state != State.Empty ||
+            b[2].state == b[4].state && b[4].state == b[6].state && b[4].state != State.Empty) {
+            return true;
+        }
+        return false;
+    };
+    Board.prototype.draw = function () {
+        for (var c in this.board) {
+            if (this.board[c].state == State.Empty) {
+                return false;
+            }
+        }
+        return true;
+    };
+    Board.prototype.resetGame = function () {
+        for (var index = 0; index < this.board.length; index++) {
+            this.table[index].innerHTML = "";
+            this.board[index].SetState(State.Empty);
+        }
+    };
+    Board.prototype.printScore = function () {
+        this.score.innerHTML = "Gracz 1: " + this.p1Score + "  Gracz 2: " + this.p2Score;
     };
     return Board;
 }());
-var board = new Board();
+window.onload = function () {
+    var cell0 = document.getElementById("cell0");
+    var cell1 = document.getElementById("cell1");
+    var cell2 = document.getElementById("cell2");
+    var cell3 = document.getElementById("cell3");
+    var cell4 = document.getElementById("cell4");
+    var cell5 = document.getElementById("cell5");
+    var cell6 = document.getElementById("cell6");
+    var cell7 = document.getElementById("cell7");
+    var cell8 = document.getElementById("cell8");
+    var buttonArray = [cell0, cell1, cell2, cell3, cell4, cell5, cell6, cell7, cell8];
+    var game = new Board(buttonArray);
+    var _loop_1 = function (index) {
+        buttonArray[index].addEventListener('click', function () { game.click(index); });
+    };
+    for (var index = 0; index < buttonArray.length; index++) {
+        _loop_1(index);
+    }
+};
--- tictactoe/tictactoe.ts
+++ tictactoe/tictactoe.ts
@@ -1,50 +1,140 @@
-class Cell {
-	cellValue: number;
-	htmlElement: HTMLElement;
-	constructor(cell: HTMLElement) {
-		this.htmlElement = cell;
-	}
+enum State {
+    Empty,
+    X,
+    O,
+}
 
-	SetStatus (status:number) {
-		this.cellValue = status;
-		if (status === -1) {
-			this.htmlElement.innerHTML = "O";
-		}
-		else if (status === 1) {
-			this.htmlElement.innerHTML = "X";
-		}
-	}
+class Cell {
+    state:State;   
+    constructor() {
+        this.state = State.Empty;
+    }
+    public SetState(s:State) {
+        this.state = s;
+    }
 }
 
 class Board {
-	cells: Cell[];
-	currentSymbol: number;
+    board:Array<Cell>;
+    table:HTMLElement[];
+    score:HTMLElement;
+    p1Turn:boolean;
+    p1Score:number;
+    p2Score:number;
 
-	constructor() {
-		this.currentSymbol = -1;
-		this.cells = new Array(9);
-		this.cells[0] = new Cell (<HTMLElement>document.getElementById("cell11"));
-		this.cells[1] = new Cell (<HTMLElement>document.getElementById("cell12"));
-		this.cells[2] = new Cell (<HTMLElement>document.getElementById("cell13"));
-		this.cells[3] = new Cell (<HTMLElement>document.getElementById("cell21"));
-		this.cells[4] = new Cell (<HTMLElement>document.getElementById("cell22"));
-		this.cells[5] = new Cell (<HTMLElement>document.getElementById("cell23"));
-		this.cells[6] = new Cell (<HTMLElement>document.getElementById("cell31"));
-		this.cells[7] = new Cell (<HTMLElement>document.getElementById("cell32"));
-		this.cells[8] = new Cell (<HTMLElement>document.getElementById("cell33"));
-	}
-	makeMove(cellNumber: number) {
-		this.cells[cellNumber].SetStatus(this.currentSymbol);
-		this.currentSymbol = this.currentSymbol === -1 ? 1 : -1;
-		return Board;
-	}
-	//clickHandler(){
-	//	for(let i = 0; i< this.cells.length ; i++){
-	//		this.??.addEventListener('click', ()=> board.makeMove())
-	//	}
-	//}
+    constructor(t:HTMLElement[]) {
+        this.table = t;
+        this.p1Turn = true;
+        this.board = [new Cell, new Cell, new Cell, new Cell, new Cell, new Cell, new Cell, new Cell, new Cell];
+        this.score = document.getElementById("score") as HTMLElement;
+        this.p1Score = 0;
+        this.p2Score = 0;
+    }
+
+    click(c:number) {
+        
+        if (this.board[c].state == State.Empty)
+        {
+            if (this.p1Turn)
+            {
+                this.board[c].SetState(State.X);
+                this.table[c].innerHTML = "X";            
+            }
+            else
+            {
+                this.board[c].SetState(State.O);
+                this.table[c].innerHTML = "O";            
+            }
+            if (this.hWin(this.board) || this.vWin(this.board) || this.dWin(this.board))
+            {              
+                if (this.p1Turn == true)
+                {
+                    this.p1Score++;
+                }
+                else
+                {
+                    this.p2Score++;
+                }
+                this.printScore();
+                this.resetGame();
+            }
+            if (this.draw())
+            {
+                this.resetGame();
+            }
+        this.p1Turn = !this.p1Turn;
+        }
+        
+    }
+
+    hWin(b:Array<Cell>) {
+        if (b[0].state == b[1].state && b[1].state == b[2].state && b[1].state != State.Empty ||
+            b[3].state == b[4].state && b[4].state == b[5].state && b[4].state != State.Empty ||
+            b[6].state == b[7].state && b[7].state == b[8].state && b[7].state != State.Empty )
+            {
+                return true;
+            }            
+        return false;
+    }
+
+    vWin(b:Array<Cell>) {
+        if (b[0].state == b[3].state && b[3].state == b[6].state && b[3].state != State.Empty ||
+            b[1].state == b[4].state && b[4].state == b[7].state && b[4].state != State.Empty ||
+            b[2].state == b[5].state && b[5].state == b[8].state && b[5].state != State.Empty)
+            {
+                return true;
+            }
+            return false;
+    }
+    dWin(b:Array<Cell>) {
+        if (b[0].state == b[4].state && b[4].state == b[8].state && b[4].state != State.Empty ||
+            b[2].state == b[4].state && b[4].state == b[6].state && b[4].state != State.Empty)
+            {
+                return true;
+            }
+            return false;
+    }
+    draw() {
+        for (var c in this.board)
+        {
+            if(this.board[c].state == State.Empty)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    resetGame() {
+        for (let index = 0; index < this.board.length; index++)
+        {
+            this.table[index].innerHTML = "";
+            this.board[index].SetState(State.Empty);
+        }
+    }
+    printScore() {
+        this.score.innerHTML = "Gracz 1: " + this.p1Score + "  Gracz 2: " + this.p2Score;
+    }
+
+
 }
-let board = new Board();
 
+window.onload = () => {
+    let cell0 = <HTMLElement> document.getElementById("cell0");
+    let cell1 = <HTMLElement> document.getElementById("cell1");
+    let cell2 = <HTMLElement> document.getElementById("cell2");
+    let cell3 = <HTMLElement> document.getElementById("cell3");
+    let cell4 = <HTMLElement> document.getElementById("cell4");
+    let cell5 = <HTMLElement> document.getElementById("cell5");
+    let cell6 = <HTMLElement> document.getElementById("cell6");
+    let cell7 = <HTMLElement> document.getElementById("cell7");
+    let cell8 = <HTMLElement> document.getElementById("cell8");
 
+    let buttonArray:HTMLElement[] = [cell0, cell1, cell2, cell3, cell4, cell5, cell6, cell7,  cell8];
+    let game = new Board(buttonArray);
 
+    for (let index = 0; index < buttonArray.length; index++) {
+        buttonArray[index].addEventListener('click',  function() {game.click(index)});
+        
+    }
+    
+}
